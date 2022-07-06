00584e76639ed12b8982f8c6299057642e1fe523
Vansh <vansh8755381346@gmail.com>
2022-07-06 14:02:49 +0530

    make it better

--- app.js
+++ app.js
@@ -19,16 +19,9 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json())
 
-app.get("/",async(req,res)=>{
-    try{
-        res.send("hello its working")
-    }
-    catch(e){
-        res.send('not working '+ e)
-    }
-})
 
-app.get("/dataget", async(req,res)=>{
+
+app.get("/", async(req,res)=>{
     
     try{
         const waiting = await Name.find()
